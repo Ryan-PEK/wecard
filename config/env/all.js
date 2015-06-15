39db200ef7d2b59826cfe3d4124eaf7554d444b9
云笈 <yunji.ll@alibaba-inc.com>
2015-06-15 13:34:15 +0800

    change default port 4401

--- config/env/all.js
+++ config/env/all.js
@@ -6,7 +6,7 @@ module.exports = {
 		description: 'wecar',
 		keywords: 'wecard'
 	},
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 4401,
 	templateEngine: 'swig',
 	sessionSecret: 'MEAN',
 	sessionCollection: 'sessions',
